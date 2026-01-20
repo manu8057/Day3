42c420a11c4a34aa1638e1e52e8717c58780d911
apnacollege <manupandit0022@gmail.com>
2026-01-20 11:49:12 +0530

    Mongodb Connected

---
+++ src/main/java/com/example/day3sms/Controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.example.day3sms.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/")
+    public String student(){
+        return "Home page for student";
+    }
+}
