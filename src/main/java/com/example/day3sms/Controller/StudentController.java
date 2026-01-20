1f010d7220437ee87221e6f5b799ff36d575dcc6
apnacollege <manupandit0022@gmail.com>
2026-01-20 14:08:44 +0530

    mongodb connected

--- src/main/java/com/example/day3sms/Controller/StudentController.java
+++ src/main/java/com/example/day3sms/Controller/StudentController.java
@@ -2,7 +2,7 @@ package com.example.day3sms.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//hnji
 @RestController
 public class StudentController {
     @GetMapping("/")
